90353b65e31ca4141afd3fab27eaea9071bc4ba5
aurakresa <aurakresa12@gmail.com>
2026-05-12 11:04:43 +0700

    initiate backend vibecheck

---
+++ src/test/kotlin/com/vibecheck/vibecheck_backend/VibecheckBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vibecheck.vibecheck_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VibecheckBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
