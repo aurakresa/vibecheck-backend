683294130a247f5cdd7698d20ffe1e45d51a9706
aurakresa <aurakresa12@gmail.com>
2026-06-03 20:03:58 +0700

    Merge branch auth and exception ke development

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,24 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+// Firebase Admin (Hanya untuk akses Firestore Database)
+	implementation("com.google.firebase:firebase-admin:9.2.0")
+
+// Google API Client (Untuk verifikasi Token Google OAuth dari Client)
+	implementation("com.google.api-client:google-api-client:2.2.0")
+
+// JJWT (Untuk Spring Boot membuat JWT sendiri)
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 }
 
 kotlin {
